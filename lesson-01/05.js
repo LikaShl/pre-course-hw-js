d92d1f510a157784c5c28a84e5876726b2470301
LikaShl <anzhelikashleiko@gmail.com>
2024-01-15 15:25:30 +0100

    add 06.js file

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -3,9 +3,11 @@ switch (true) {
     case rule.length < 25: 
      console.log("Все таки нет правил без исключения"); 
      break; 
+
      case rule.length > 25: 
      console.log("У меня 100 пудов все получится"); 
      break; 
+     
    default:
     console.log(«50 на 50»); 
    }
