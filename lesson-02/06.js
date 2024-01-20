9a25d42b9b56ce4701f6511f46d9f29ff377d6ef
Anzhelika <AnzhelikaShleiko@gmail.com>
2024-01-20 22:52:40 +0100

    ex.2/6

---
+++ lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+let deep = structuredClone(passportWithAddress);
+
+deep.address.city = "Bobryisk";
+console.log(passportWithAddress.address.city);
+console.log(deep.address.city);
