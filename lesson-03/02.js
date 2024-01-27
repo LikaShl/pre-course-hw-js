985856d177f44e94121eb37dc425d5ad8760f415
Anzhelika <AnzhelikaShleiko@gmail.com>
2024-01-27 14:26:16 +0100

    ex4

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,2 +1,2 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-let orange = words.indexOf("апельсин") ? console.log("Ура! нашел") : console.log("Придется поискать в другом магазине…")
+let orange = words.indexOf("апельсин") ? console.log ("Ура! нашел") : console.log("Придется поискать в другом магазине…")
